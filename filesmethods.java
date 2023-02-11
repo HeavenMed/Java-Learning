a054f1be4818734a176a570bb683a3b026795484
Medeiros <guilherme.ggcm20@gmail.com>
2023-02-11 13:25:42 -0300

    File Methods Learning

---
+++ filesmethods.java
@@ -0,0 +1,74 @@
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;   // Import the FileWriter class
+
+public class filesmethods {
+    public static void main(String[] args){
+        //File handling is an important part of any application.
+        //Java has several methods for creating, reading, updating, and deleting files.
+        //The File class from the java.io package, allows us to work with files.
+        File myObj = new File("filename.txt"); // Specify the filename
+       //canRead()	Boolean	Tests whether the file is readable or not
+       //canWrite()	Boolean	Tests whether the file is writable or not
+        //createNewFile()	Boolean	Creates an empty file
+        ///delete()	Boolean	Deletes a file
+        //exists()	Boolean	Tests whether the file exists
+        //getName()	String	Returns the name of the file
+        //getAbsolutePath()	String	Returns the absolute pathname of the file
+        //list()	String[]	Returns an array of the files in the directory
+        //
+
+        //Vamos criar u marquivo
+        try{
+
+            File arquivo = new File("Arquivocriado.txt"); // para criar o objeto arquivo e 
+            //atribuir a classe File, com seus métodos, tais como criar, ai depois é necessário
+            // chamar o objeto e atribuir seu método createNewFile para criar o arquivo
+
+            if(arquivo.createNewFile()){
+                System.out.println("Arquivo criado com sucesso e seu nome é" +arquivo.getName());
+            }else{
+                System.out.println("File already exists.");
+            }
+
+        } catch(IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+        //Se quiser criar o arquivo em um diretório específico, na hora de criar o objeto com classe
+        // file, vc coloca o caminho antes do nome
+        File myObj2 = new File("C:\\Users\\MyName\\filename.txt"); // dessa forma
+
+        //In the following example, we use the FileWriter class together with its write() method to write 
+        //some text to the file we created in the example above. 
+        //Note that when you are done writing to the file, you should close it with the close() method:
+
+        try{
+            //Crie o objeto escrever com a classe FileWriter e coloque o nome do arquivo para ser escrito
+            FileWriter escrever = new FileWriter("Arquivocriado.txt");
+            //Escreva com o método write() do objeto que herdou os métodos da classe FileWriter
+            escrever.write("Files in Java might be tricky, but it is fun enough!");
+            escrever.close();
+            System.out.println("Successfully wrote to the file.");
+
+        }catch(IOException e){
+            System.out.println("An error occurred.");
+        e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
