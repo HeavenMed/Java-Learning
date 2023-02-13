96144262c45ed4917b7ba31a8be03b28c9128e6e
Medeiros <guilherme.ggcm20@gmail.com>
2023-02-13 16:13:28 -0300

    Learning Buttons

---
+++ buttons.java
@@ -0,0 +1,28 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel; // importando JLabel
+import javax.swing.JPanel; // 
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font; 
+
+public class buttons {
+
+
+    public static void main(String[] args) {
+   
+        //JButton = a button that performs an action when clicked on
+
+        
+
+        JButton button = new JButton()
+
+
+
+
+
+
+    }
+}
--- learningpanels.java
+++ learningpanels.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel; // importando JLabel
+import javax.swing.JPanel; // 
 import javax.swing.border.Border;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -9,11 +10,42 @@ import java.awt.Font;
 public class learningpanels {
     public static void main(String[] args){  
 
+        //JPanel = a GUI component that functions as a container to hold other components
+        ImageIcon icon = new ImageIcon("download.jfif");
+
+
+        JLabel label = new JLabel();
+        label.setText("Hi emojo");
+        label.setIcon(icon);
+
+
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0 , 0 , 250 , 250);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250 , 0 , 250 , 250);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0 , 250 , 500 , 250);
+
+
+
         JFrame frame = new JFrame(); // Creates a frame
             frame.setTitle("JFRAME TÍTULO"); // setando o título
+            frame.setLayout(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Encerra quando o cliente fecha a janela, pode ter outros módulos
-            frame.setSize(420, 420); //sets the x-dimension and y dimension
+            frame.setSize(750, 750); //sets the x-dimension and y dimension
             frame.setVisible(true);// makes frame visible
+        
+        frame.add(redPanel);
+        redPanel.add(label);
+
+        frame.add(bluePanel);
+        frame.add(greenPanel);
 
 
 
