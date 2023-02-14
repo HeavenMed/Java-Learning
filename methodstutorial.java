f4a313b54e742eb9ff815410b34671850d47a9d9
Medeiros <guilherme.ggcm20@gmail.com>
2023-02-14 19:18:43 -0300

    Estudando Métodos

---
+++ methodstutorial.java
@@ -0,0 +1,37 @@
+public class methodstutorial {
+    public static void main(String[] args){ 
+
+        // O que é um método?
+            //É uma porção de código que uma classe disponbiliza, ele é executado quando é feito uma 
+            // requisição, todo método está dentro de uma classe e realiza um determinado comportamento
+
+        //Definir e Utilizar 
+            //Padrão de Definição
+            //<?Visiblidade?><?Tipo?><?modificar?>  Retorno 
+            // nome (<?parâmetros?>) <?exceções?> {corpo}
+            // os que estão entre ? são opcionais
+            //V: Public, protected ou private
+            // T: concreto ou abstrato
+            //M : Static ou final
+            //R : Tipo de dado/retorno ou sem retorno ( void ) , ele executa os códigos internos e nada mais
+            // N: NOME
+            // P: PARÂMETROS, E : EXCEÇÕES E CORPO: CÓDIGO
+
+            //Para utilizar um método, chame-o
+            // Nome_da_classe.nome_do_método();
+            // Como usar a ASSINATURA em um método?
+            // assinatura é uma forma de identificar unicamente o método
+            // assinatura é : nome + seus parâmetros
+            //Construtor e Destrutor são tipos especiais de métodos usados na Orientação a Obj
+
+            //Como criar sobrecarga?
+            //Sobrecarga é a capacidade de definir métodos para diferentes contextos, mas preservando seu nome
+            // Para iss oacontecer, devemos mudar seus parâmetros, sua assinatura
+            
+
+
+
+
+
+    }
+}
